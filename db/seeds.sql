89c80d1771df9add29accbcba061a0819c83398b
Max Yao <bojinyao@berkeley.edu>
2018-08-23 20:31:39 -0700

    Set up everything, ready to start testing and debug.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+insert into burgers(burger_name, devoured) values 
+    ("cheese burger", false),
+    ("double cheese burger", false),
+    ("triple cheese burger", false),
+    ("classic chicken burger", false);
+
